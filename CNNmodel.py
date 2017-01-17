9ae840a6f98d1daad1ea54d004b8ea0ba4b34ae7
Rashmeet <mail2rashmeet9@gmail.com>
2017-01-17 11:26:07 +0530

    adding CNN model

---
+++ CNNmodel.py
@@ -0,0 +1,105 @@
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+from keras import backend as K
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import ELU
+from keras.utils.data_utils import get_file
+from keras.layers import Input, Dense
+from keras.layers.core import Activation, Reshape
+
+TH_WEIGHTS_PATH = 'https://github.com/keunwoochoi/music-auto_tagging-keras/blob/master/data/music_tagger_cnn_weights_theano.h5'
+TF_WEIGHTS_PATH = 'https://github.com/keunwoochoi/music-auto_tagging-keras/blob/master/data/music_tagger_cnn_weights_tensorflow.h5'
+
+
+def MusicTaggerCNN(weights=None, input_tensor=None,
+                   include_top=True):
+
+    if weights not in {'msd', None}:
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization) or `msd` '
+                         '(pre-training on Million Song Dataset).')
+
+    # Determine proper input shape
+    if K.image_dim_ordering() == 'th':
+        input_shape = (96, 1366)          #channels, rows, columns
+    else:
+        input_shape = (96, 1366, 1)
+
+    if input_tensor is None:
+        melgram_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            melgram_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            melgram_input = input_tensor
+
+
+    # Determine input axis
+    if K.image_dim_ordering() == 'th':
+        channel_axis = 1
+        freq_axis = 2
+        time_axis = 3
+    else:
+        channel_axis = 3
+        freq_axis = 1
+        time_axis = 2
+  
+    x = Reshape((1,) + input_shape , input_shape=input_shape) (melgram_input)
+
+    # Input block
+    #x = BatchNormalization(axis=time_axis, name='bn_0_freq')(melgram_input)
+
+    # Conv block 1
+    x = Convolution2D(32, 3, 3, border_mode='same', name='conv1')(x)
+    x = BatchNormalization(axis=channel_axis, mode=0, name='bn1')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(2, 4), name='pool1')(x)
+
+    # Conv block 2
+    x = Convolution2D(128, 3, 3, border_mode='same', name='conv2')(x)
+    x = BatchNormalization(axis=channel_axis, mode=0, name='bn2')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(2, 4), name='pool2')(x)
+
+    # Conv block 3
+    x = Convolution2D(128, 3, 3, border_mode='same', name='conv3')(x)
+    x = BatchNormalization(axis=channel_axis, mode=0, name='bn3')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(2, 4), name='pool3')(x)
+
+    # Conv block 4
+    x = Convolution2D(192, 3, 3, border_mode='same', name='conv4')(x)
+    x = BatchNormalization(axis=channel_axis, mode=0, name='bn4')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(3, 5), name='pool4')(x)
+
+    # Conv block 5
+    x = Convolution2D(256, 3, 3, border_mode='same', name='conv5')(x)
+    x = BatchNormalization(axis=channel_axis, mode=0, name='bn5')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(4, 4), name='pool5')(x)
+
+    # Output
+    x = Flatten()(x)
+    if include_top:
+        x = Dense(37, activation='sigmoid', name='output')(x)
+
+    # Create model
+    model = Model(melgram_input, x)
+    if weights is None:
+        return model    
+    else: 
+        # Load input
+        if K.image_dim_ordering() == 'tf':
+            raise RuntimeError("Please set image_dim_ordering == 'th'."
+                               "You can set it at ~/.keras/keras.json")
+        model.load_weights('data/music_tagger_cnn_weights_%s.h5' % K._BACKEND,
+                           by_name=True)
+        return model
